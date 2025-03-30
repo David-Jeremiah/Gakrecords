2a16abbe695675a21a0141a9a4d1902926b496a1
David-Jeremiah <davidndyagendajeremiah2017@gmail.com>
2025-03-30 23:45:10 +0300

    final work

--- web/gakrecords/urls.py
+++ web/gakrecords/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gakrecords.apps.codehigh.urls')),
-    path('gakstudio/', include('gakrecords.apps.gakstudio.urls')),
 ]
 
 if settings.DEBUG:
